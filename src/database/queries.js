37f0a2a1a4756d5b2aa5f59bc0645c4ab095d714
Glenn Lee <glenn@Glenns-MacBook-Pro.local>
2021-03-14 06:33:29 +0800

    Add Database

---
+++ src/database/queries.js
@@ -0,0 +1,19 @@
+import 'firebase.js';
+
+export function validateLogin(username, password) {
+    return database.collection('users').doc(username).get().then(snapshot => {
+        if (!snapshot.exists) {
+            return false;
+        }
+
+        const userData = snapshot.data();
+
+        if (password != userData.password) {
+            return false;
+        }
+
+        return true;
+    })
+}
+
+
