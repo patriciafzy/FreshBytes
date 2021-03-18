87a47180921bd95945a304bf0ace4bafc9adb181
patriciafzy <e0420532@u.nus.edu>
2021-03-18 14:14:35 +0800

    updated about us

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,70 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About Us</h1>
+    <h2>Less Waste, Better Taste</h2>
+    <p>
+      We connect you to F&B businesses and local supermarket 
+      who have fresh but excess meals or products for sale and
+      consumption at discounted rates.
+    </p>
+    <p>
+      We believe that every meal saved is a step closer towards
+      saving our Earth.
+    </p>
+    <br>
+    <p>
+      Food waste is one of the biggest waste streams in Singapore
+      and the amount of food waste generated has grown by around
+      <b>20% over the last 10 years.</b>
+    </p>
+    <p>
+      In 2019, Singapore generated around 744 million kg of food
+      waste. That is equivalent to 2 bowls of rice per person per
+      day, or around 51,000 double decker buses.
+    </p>
+    <br><br><br>
+    <h2>Our Partners</h2>
+        <img src="../assets/img/freshpartners.png" height=200px><br><br><br><br>
+    <h2>Our Team</h2><br>
+        <ul>
+            <li><img src="../assets/img/darren.jpeg" height=400px width=400px><p><b>Darren</b></p></li>
+            <li><img src="../assets/img/erica.jpeg" height=400px width=400px><p><b>Erica</b></p></li>
+            <li><img src="../assets/img/patricia.jpeg" height=400px width=400px><p><b>Patricia</b></p></li>
+            <li><img src="../assets/img/glenn.jpeg" height=400px width=400px><p><b>Glenn</b></p></li>
+          <!-- <li><img src="../assets/freshpartners.png" height=200px></li>
+          <li><img src="../assets/freshpartners.png" height=200px></li> -->
+        </ul>
+    
   </div>
 </template>
+
+<script> 
+// import freshpartner from "../assets/freshpartners.png"
+
+// export default {
+//     data: function () {
+//         return {
+//             freshpartner: freshpartner
+//         }
+//     }
+// }
+</script>
+
+<style>
+ul {
+  display: flex;
+  flex-wrap: wrap;
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  flex-grow: 1;
+  flex-basis: 500px;
+  text-align: center;
+  padding: 10px;  
+  margin: 10px;
+}
+
+
+</style>
