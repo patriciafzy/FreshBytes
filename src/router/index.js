5de6e84ce0a06bff70cc5f0cdcac03262c117089
ericajanetem <ariecarh@gmail.com>
2021-03-19 17:30:19 +0800

    Add new routes for AddListing, ReviewListing

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import AddListing from '../components/AddListing.vue'
+import ReviewListing from '../components/ReviewListing.vue'
 
 Vue.use(VueRouter)
 
@@ -17,6 +19,17 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/addlisting',
+    name: 'Add Listing',
+    component: AddListing
+  },
+  {
+    path: '/reviewListing',
+    name: 'Review Listing',
+    component: ReviewListing,
+    props: true,
   }
 ]
 
