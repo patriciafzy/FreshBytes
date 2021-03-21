b51bb50f7b88f66b3d55fa2a1810fb4bc63d67f7
patriciafzy <e0420532@u.nus.edu>
2021-03-21 20:22:53 +0800

    Added new routes for ProductList, IndividualProduct

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import ProductList from '../views/ProductList.vue'
+import Product from '../components/Product.vue'
 
 Vue.use(VueRouter)
 
@@ -17,7 +19,18 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/ProductList',
+    name: 'ProductList',
+    component: ProductList,
+  },
+  {
+    path: '/products/:id',
+    // path: '/products',
+    name: 'product',
+    component: Product,
+  },
 ]
 
 const router = new VueRouter({
