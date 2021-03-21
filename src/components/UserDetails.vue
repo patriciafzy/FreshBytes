d4d9ad272338c717dfc7275922f6361dd68da9f9
dlsh <dlsh3858@gmail.com>
2021-03-21 21:20:09 +0800

    added form

--- src/components/UserDetails.vue
+++ src/components/UserDetails.vue
@@ -7,8 +7,31 @@
             <p>Username: <span>{{this.userDetails.username}}</span></p>
             <p>Points: <span>{{this.userDetails.points}}</span></p>
             <p>Location: <span>{{this.userDetails.location}}</span></p>
+            <button type = "button" v-on:click = "toggleForm">Change Details</button>
         </div>
-        <button type = "button">Change Details</button>
+        <div v-if = "isEdit">
+            <form action = "">
+                <label for = "name">Name: </label>
+                <input type = "text" name = "name" v-bind:value = "userDetails.name">
+                <br><br>
+                <label for = "email">Email: </label>
+                <input type = "email" name = "email" v-bind:value = "userDetails.email">
+                <br><br>
+                <label for = "username">Username: </label>
+                <input type = "text" name = "username" v-bind:value = "userDetails.username">
+                <br><br>
+                <label for = "location">Location: </label>
+                <select name = "location" v-bind:value = "userDetails.location">
+                    <option value = "North">North</option>
+                    <option value = "South">South</option>
+                    <option value = "East">East</option>
+                    <option value = "West">West</option>
+                </select>
+                <br><br>
+                <button type = "button" v-on:click = "toggleForm">Cancel Changes</button>
+                <button type = "submit">Submit Not Done D:</button>
+            </form>
+        </div>
     </div>
 </template>
 
@@ -23,7 +46,9 @@ export default {
         userDetails: {}
     },
     methods: {
-
+        toggleForm: function() {
+            this.isEdit = !this.isEdit;
+        }
     }
     
 }
@@ -34,11 +59,11 @@ export default {
         border: green solid 2px;
         text-align: left;
     }
-    p {
+    p, label {
         font-weight: bold;
     }
 
-    span {
+    span, input, select {
         font-weight: normal;
     }
 
