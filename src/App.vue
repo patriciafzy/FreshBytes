16c19392f8f6af1197181141ac9ccd8a98034e8a
Glenn Lee <glenn@Glenns-MacBook-Pro.local>
2021-03-23 02:30:44 +0800

    Fix Layouts

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,8 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/profile">Profile</router-link>
-      <router-link to="/ProductList">Products</router-link> |
+      <router-link to="/ProductList">Products</router-link> | 
+      <router-link to="/profile">Profile</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
@@ -22,13 +21,6 @@
 
 #nav {
   padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  } 
 }
 
 #nav a {
