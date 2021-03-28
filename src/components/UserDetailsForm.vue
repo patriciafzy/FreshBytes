a614ad2005d6906a6ff6cc073d7da5284a920e5d
dlsh <dlsh3858@gmail.com>
2021-03-29 04:05:12 +0800

    Replace form fields with buefy versions

--- src/components/UserDetailsForm.vue
+++ src/components/UserDetailsForm.vue
@@ -1,36 +1,44 @@
 <template>
   <div>
     <form action="">
-      <label for="name">Name: </label>
-      <input type="text" name="name" v-bind:value="userDetails.name" />
-      <label for="email">Email: </label>
-      <input type="email" name="email" v-bind:value="userDetails.email" />
-      <label for="username">Username: </label>
-      <input type="text" name="username" v-bind:value="userDetails.username" />
+      <b-field label="Name">
+        <b-input :value="userDetails.name"></b-input>
+      </b-field>
+      <b-field label="Email">
+        <b-input
+          type="email"
+          :value="userDetails.email"
+          validation-message="This is not a valid email"
+        ></b-input>
+      </b-field>
+      <b-field
+        label="Username"
+        :type="{ 'is-success': userNameAvailable }"
+        :message="{ 'This username is available': userNameAvailable }"
+      >
+        <b-input :value="userDetails.username" maxlength="30"></b-input>
+      </b-field>
       <div v-if="userType == 'customer'">
-        <label for="location">Location: </label>
-        <select name="location" v-bind:value="userDetails.location">
-          <option value="North">North</option>
-          <option value="South">South</option>
-          <option value="East">East</option>
-          <option value="West">West</option>
-        </select>
+        <b-field label="Location">
+          <b-select :value="userDetails.location">
+            <option value="North">North</option>
+            <option value="South">South</option>
+            <option value="East">East</option>
+            <option value="West">West</option>
+          </b-select>
+        </b-field>
       </div>
       <div v-else>
-        <label for="address">Address: </label>
-        <textarea
-          name="address"
-          rows="2"
-          cols="20"
-          v-bind:value="userDetails.address"
-        ></textarea>
-        <label for="description">Description: </label>
-        <textarea
-          name="description"
-          rows="2"
-          cols="20"
-          v-bind:value="userDetails.description"
-        ></textarea>
+        <b-field label="Address">
+          <b-input :value="userDetails.address"></b-input>
+        </b-field>
+        <b-field label="Description">
+          <b-input
+            maxlength="200"
+            type="textarea"
+            :value="userDetails.description"
+          ></b-input>
+        </b-field>
       </div>
       <button type="submit">Submit Not Done D:</button>
     </form>
@@ -39,6 +47,11 @@
 
 <script>
 export default {
+  data() {
+    return {
+      usernameAvailable: false,
+    };
+  },
   props: {
     userDetails: Object,
     userType: String,
@@ -51,18 +64,14 @@ label {
   display: block;
   margin-top: 10px;
   font-weight: bold;
-  margin-left: 20px;
 }
 
 button {
-  margin: 20px;
+  margin-top: 20px;
   display: block;
 }
 
-input,
-select,
-textarea {
-  font-weight: normal;
+form {
   margin-left: 20px;
 }
 </style>
