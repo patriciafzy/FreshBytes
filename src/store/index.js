a8e1608b3121198b304300406d20c07b187a2db1
dlsh <dlsh3858@gmail.com>
2021-03-30 01:59:31 +0800

    Add functionality: Keep login even after refresh

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,22 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import db from "../database/firebase.js";
+import createPersistedState from "vuex-persistedstate";
+import SecureLS from "secure-ls";
+var ls = new SecureLS({ isCompression: false });
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
+  plugins: [
+    createPersistedState({
+      storage: {
+        getItem: (key) => ls.get(key),
+        setItem: (key, value) => ls.set(key, value),
+        removeItem: (key) => ls.remove(key),
+      },
+    }),
+  ],
   state: {
     isLoggedIn: false,
     userData: {
