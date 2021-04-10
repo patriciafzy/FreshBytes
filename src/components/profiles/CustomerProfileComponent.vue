e3fc763e93f9137be411e5a55a829b4956e195b5
Glenn Lee <glenn@Glenns-MacBook-Pro.local>
2021-04-11 05:55:00 +0800

    Change Database Structure and Added Profile Pages

--- src/components/profiles/CustomerProfileComponent.vue
+++ src/components/profiles/CustomerProfileComponent.vue
@@ -1,10 +1,14 @@
 <template>
-  <div v-if="userDetails">
+  <div>
+    This is a customer profile page that is tempoarily broken.... Error 404 that
+    kind of stuff.
+  </div>
+  <!-- <div v-if="userDetails">
     <h1 class="title is-3">{{ userDetails.name }}'s Profile Page</h1>
     <side-bar v-on:change-profile="changeDetails"></side-bar>
     <div id="content">
       <user-details
-        v-bind:userDetails="userDetails"
+        v-bind:userDetails="userData"
         v-bind:isCustomer="isCustomer"
         v-bind:userId="userId"
         v-if="toShow.details"
@@ -14,60 +18,48 @@
         v-if="toShow.orders"
       ></order-details>
     </div>
-  </div>
+  </div> -->
 </template>
 
 <script>
-import SideBar from "../SideBar.vue";
-import OrderDetails from "../OrderDetails.vue";
-import UserDetails from "../UserDetails.vue";
-import {
-  getUserDetails,
-  getUserOrdersWithListing,
-} from "../../database/queries.js";
+// import SideBar from "../SideBar.vue";
+// import OrderDetails from "../OrderDetails.vue";
+// import UserDetails from "../UserDetails.vue";
 
-export default {
-  data() {
-    return {
-      userDetails: null,
-      isCustomer: Boolean,
-      allOrders: [],
-      toShow: {
-        details: true,
-        orders: false,
-        dashboard: false,
-      },
-      userId: String,
-    };
-  },
-  methods: {
-    /**
-     * Changes the displayed element in the profile page based on a click on the sidebar
-     */
-    changeDetails: function (event) {
-      for (const property in this.toShow) {
-        this.toShow[property] = property == event ? true : false;
-      }
-    },
-  },
-  components: {
-    SideBar,
-    UserDetails,
-    OrderDetails,
-  },
-  created: function () {
-    const username = this.$store.getters.getUsername;
-    this.isCustomer = this.$store.getters.getUserType;
-    getUserDetails(username, this.isCustomer).then((doc) => {
-      this.userDetails = doc.data();
-      this.userId = doc.id;
-
-      getUserOrdersWithListing(this.isCustomer, doc.id).then((orders) => {
-        this.allOrders = orders;
-      });
-    });
-  },
-};
+// export default {
+//   data() {
+//     return {
+//       isCustomer: Boolean,
+//       allOrders: [],
+//       toShow: {
+//         details: true,
+//         orders: false,
+//         dashboard: false,
+//       },
+//       userId: String,
+//     };
+//   },
+//   computed: {
+//     userData: function () {
+//       return this.$store.getters.getUserData;
+//     }
+//   },
+//   methods: {
+//     /**
+//      * Changes the displayed element in the profile page based on a click on the sidebar
+//      */
+//     changeDetails: function (event) {
+//       for (const property in this.toShow) {
+//         this.toShow[property] = property == event ? true : false;
+//       }
+//     },
+//   },
+//   components: {
+//     SideBar,
+//     UserDetails,
+//     OrderDetails,
+//   },
+// };
 </script>
 
 <style scoped>
