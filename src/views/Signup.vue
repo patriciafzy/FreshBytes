2d1d48fbb1701fa7e403b2b2a0c0b46db8b0812b
ericajanetem <ariecarh@gmail.com>
2021-04-14 01:26:47 +0800

    Add styling for sign up page

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -6,21 +6,46 @@
         height="300px"
         width="300px"
       />
-      <h1 class="title is-3">Sign Up</h1>
       <div v-if="!customer && !business">
-        <b-button
-          type="is-primary"
-          rounded
-          expanded
-          v-on:click="toggleCustomer"
-        >
-          Customer
-        </b-button>
-        <b-button type="is-danger" rounded expanded v-on:click="toggleBusiness">
-          Business
-        </b-button>
+        <h1 class="title is-3">Sign Up</h1>
+        <h1 class="subtitle is-6">Join us and make a difference today!</h1>
+        <h1 class="title is-4">Select Your Account Type</h1>
+        <div class="container">
+          <div class="card-image">
+            <figure class="image is-1by1">
+              <button v-on:click="toggleCustomer">
+                <img :src="CustomerImage" id="buttonimg" />
+              </button>
+            </figure>
+            <b-button
+              focused
+              type="is-primary"
+              expanded
+              v-on:click="toggleCustomer"
+            >
+              Customer
+            </b-button>
+          </div>
+          <div class="card-image">
+            <figure class="image is-1by1">
+              <button v-on:click="toggleBusiness">
+                <img :src="BusinessImage" id="buttonimg" />
+              </button>
+            </figure>
+            <b-button
+              focused
+              type="is-danger"
+              expanded
+              v-on:click="toggleBusiness"
+            >
+              Business
+            </b-button>
+          </div>
+        </div>
       </div>
       <div v-if="customer">
+        <h1 class="title is-3">Sign Up</h1>
+        <h1 class="subtitle is-5">Start Shopping with us today!</h1>
         <customer-signup />
       </div>
     </div>
@@ -29,6 +54,8 @@
 
 <script>
 import CustomerSignup from "../components/signups/CustomerSignup.vue";
+import BusinessImage from "../assets/designs/businessman.png";
+import CustomerImage from "../assets/designs/shopping.jpeg";
 
 export default {
   components: {
@@ -38,6 +65,8 @@ export default {
     return {
       customer: false,
       business: false,
+      BusinessImage: BusinessImage,
+      CustomerImage: CustomerImage,
     };
   },
   methods: {
@@ -56,5 +85,16 @@ export default {
 <style scoped>
 button {
   margin-top: 5px;
+}
+
+.container {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  grid-gap: 0 10em;
+}
+
+#buttonimg {
+  border: 3px solid #8c906d;
+  border-radius: 20%;
 }
 </style>
