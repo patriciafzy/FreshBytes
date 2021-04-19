a8c79e1574857867460c3067fc06dca14004bb52
dlsh <dlsh3858@gmail.com>
2021-04-20 02:58:49 +0800

    Add Edit Listing Functionality

--- src/firebase/storage.js
+++ src/firebase/storage.js
@@ -12,5 +12,19 @@ export const uploadImage = async (image, imageName) => {
   return imageRef.getDownloadURL();
 };
 
+export const deleteImage = (imageName) => {
+  const storageRef = storage.ref();
+  const imageRef = storageRef.child("images/" + imageName);
+
+  return imageRef
+    .delete()
+    .then(() => {
+      console.log("Successfully deleted image");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
 // Get Unique Identifier
 const UID = () => Math.random().toString(36).substr(2, 9) + "_";
