5bda3f97bdeb35b84edcdad2f1a5e6428939f8de
ericajanetem <ariecarh@gmail.com>
2021-04-20 03:40:28 +0800

    Update credit card for cart

--- src/views/CartPage.vue
+++ src/views/CartPage.vue
@@ -22,31 +22,55 @@
       </b-step-item>
       <b-step-item step="3" label="Payment" :clickable="false">
         <h1 class="title has-text-centered">Payment</h1>
-        Lorem ipsum dolor sit amet.
+        <credit-card-component />
       </b-step-item>
       <b-step-item step="4" label="Complete" :clickable="false">
-        <h1 class="title has-text-centered">Complete</h1>
-        Lorem ipsum dolor sit amet.
+        <complete />
       </b-step-item>
+
+      <template slot="navigation" slot-scope="{ previous, next }">
+        <div class="field is-grouped is-grouped-centered">
+          <p class="control">
+            <a class="button is-primary" @click.prevent="next.action">Submit</a>
+          </p>
+          <p class="control">
+            <a class="button is-light" @click.prevent="previous.action">Back</a>
+          </p>
+        </div>
+      </template>
     </b-steps>
   </div>
 </template>
 
 <script>
 import CartComponent from "@/components/cart/CartComponent";
+import CreditCardComponent from "@/components/cart/CreditCardComponent.vue";
+import Complete from "@/components/cart/Complete";
 import { mapGetters } from "vuex";
 
 export default {
   components: {
     CartComponent,
+    CreditCardComponent,
+    Complete,
   },
   data: function () {
     return {
       activeStep: 0,
+      submitting: false,
+      isFullPage: true,
     };
   },
   computed: {
     ...mapGetters(["getCartCount"]),
+  },
+  watch: {
+    activeStep() {
+      const loadingComponent = this.$buefy.loading.open({
+        container: this.isFullPage ? null : this.$refs.element.$el,
+      });
+      setTimeout(() => loadingComponent.close(), 1000);
+    },
   },
 };
 </script>
