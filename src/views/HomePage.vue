3a2d13822589231c5af2cb7cb0e66e02912278a5
ericajanetem <ariecarh@gmail.com>
2021-04-20 17:51:42 +0800

    Add team into home page

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -8,6 +8,31 @@
         </figure>
       </b-carousel-item>
     </b-carousel>
+    <section class="section is-small">
+      <h1 class="title is-3">Our Team</h1>
+      <h1 class="subtitle is-6">Meet the developers behind Fresh Bytes</h1>
+      <br />
+      <ul>
+        <li>
+          <img src="@/assets/img/darren.jpeg" height="300px" width="300px" />
+          <p><b>Darren</b></p>
+        </li>
+        <li>
+          <img src="@/assets/img/erica.jpeg" height="300px" width="300px" />
+          <p><b>Erica</b></p>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <img src="@/assets/img/patricia.jpeg" height="300px" width="300px" />
+          <p><b>Patricia</b></p>
+        </li>
+        <li>
+          <img src="@/assets/img/glenn.jpeg" height="300px" width="300px" />
+          <p><b>Glenn</b></p>
+        </li>
+      </ul>
+    </section>
   </div>
 </template>
 
@@ -33,5 +58,19 @@ export default {
   width: 1000px;
   margin: auto;
   margin-top: 50px;
+}
+
+ul {
+  display: flex;
+  flex-wrap: wrap;
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  flex-grow: 1;
+  flex-basis: 500px;
+  text-align: center;
+  padding: 10px;
+  margin: 10px;
 }
 </style>
